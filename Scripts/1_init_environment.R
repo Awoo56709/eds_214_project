a819a21fa019f63fbfba85fd6f737e5892941b2b
Awoo56709 <62405610+Awoo56709@users.noreply.github.com>
2025-08-29 10:13:46 -0700

    Added a branch

--- Scripts/1_init_environment.R
+++ Scripts/1_init_environment.R
@@ -1,6 +1,10 @@
 #Creating an initilization environment
 
-#Adding packages needed for analysis
-
+#Adding a vector of packages needed for analysis
 package_list <- c("lubridate","here","janitor","lubridate")
 
+for (package in package_list) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+}
---
+++ Scripts/script_test.R
@@ -0,0 +1,4 @@
+
+source("Scripts","1_init_environment.R")
+
+source("Scripts","2_read_data.R")
